a4c0852f06e8785588cd4809a0311e9dd2aced42
Ievgen Kuznietsov <ievgen.contact@gmail.com>
2024-12-28 08:36:31 +0000

    Initial Commit

---
+++ Memorize2024/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Memorize2024
+//
+//  Created by Mac on 28/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memorize2024/Memorize2024App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize2024App.swift
+//  Memorize2024
+//
+//  Created by Mac on 28/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Memorize2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
